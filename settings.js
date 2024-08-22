339f6a072e86dfde6d232676dfb212731abb54ee
earthsaf <154933783+earthsaf@users.noreply.github.com>
2024-08-23 00:27:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 global.prefa = ['','!','.',',','🐤','🗿']
 
-global.owner = ['628xxxxxxx']
+global.owner = ['2347068387332']
 global.botname = 'Clara VIP'
 global.baileys1 = require('@whiskeysockets/baileys') 
 global.sticker1 = "Clara Official"
