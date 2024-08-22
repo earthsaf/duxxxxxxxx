6bbb9ba0cd4c0debc3d8145b657dd8342f7f8a30
earthsaf <154933783+earthsaf@users.noreply.github.com>
2024-08-22 23:28:32 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['628xxxxxxx']
+global.botname = 'Clara VIP'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Clara Official"
+global.sticker2 = "😘"
